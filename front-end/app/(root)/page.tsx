91040b297ba5143d09ba675c6f721214f339306d
Chandrika Sharma <madenyachandrikasharma@gmail.com>
2025-04-10 21:10:37 +0530

    add home page layout

--- front-end/app/(root)/page.tsx
+++ front-end/app/(root)/page.tsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import HeaderBox from '@/components/HeaderBox';
+import RightSidebar from '@/components/RightSidebar';
 
 const Home = () => {
+  const loggedIn = { firstName: 'Adrian', lastName: 'JSM', email: 'contact@jsmastery.pro' };
+
   return (
-    <div>page</div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+
+        </header>
+
+        RECENT TRANSACTIONS
+      </div>
+
+      <RightSidebar 
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.50 }, { currentBalance: 500.50}]}
+      />
+    </section>
   )
 }
 
