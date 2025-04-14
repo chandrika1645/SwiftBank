92b60faeb0c555b222ead8e6e30e09b57fb2cc55
Chandrika Sharma <madenyachandrikasharma@gmail.com>
2025-04-14 23:18:58 +0530

    remaining layout sections and fix

--- front-end/app/(root)/page.tsx
+++ front-end/app/(root)/page.tsx
@@ -1,4 +1,4 @@
-import HeaderBox from '@/components/HeaderBox';
+import HeaderBox from '@/components/HeaderBox'
 import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 
@@ -9,28 +9,27 @@ const Home = () => {
     <section className="home">
       <div className="home-content">
         <header className="home-header">
-          <HeaderBox
+          <HeaderBox 
             type="greeting"
             title="Welcome"
             user={loggedIn?.firstName || 'Guest'}
             subtext="Access and manage your account and transactions efficiently."
           />
 
-          <TotalBalanceBox
+          <TotalBalanceBox 
             accounts={[]}
             totalBanks={1}
             totalCurrentBalance={1250.35}
           />
-
         </header>
 
         RECENT TRANSACTIONS
       </div>
 
-      <RightSidebar
+      <RightSidebar 
         user={loggedIn}
         transactions={[]}
-        banks={[{ currentBalance: 123.50 }, { currentBalance: 500.50 }]}
+        banks={[{ currentBalance: 123.50 }, { currentBalance: 500.50}]}
       />
     </section>
   )
